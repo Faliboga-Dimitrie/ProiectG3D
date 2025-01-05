9ed65e8d6e378844cf17696d03f81027cf6956a3
Dimitrie Faliboga <dimitrie.faliboga@student.unitbv.ro>
2025-01-05 15:52:53 +0200

    Cptured mouse; Changed flag from track model;

--- ProiectG3D/ProiectG3D.cpp
+++ ProiectG3D/ProiectG3D.cpp
@@ -99,7 +99,7 @@ int main()
 	glfwSetScrollCallback(window, scroll_callback);
 
 	// tell GLFW to capture our mouse
-	//glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
 	glewInit();
 
